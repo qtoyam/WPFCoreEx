4c0ce60113bee34b45d07cd1b7e04e1f1fa38ac1
qtoyam <qtoyam@gmail.com>
2021-12-05 06:05:16 +0300

    input handle ready

--- WPFCoreEx/Behaviors/InputMaskBehavior.cs
+++ WPFCoreEx/Behaviors/InputMaskBehavior.cs
@@ -11,65 +11,75 @@ using Microsoft.Xaml.Behaviors;
 
 namespace WPFCoreEx.Behaviors
 {
-	public enum MaskType
+	public enum MaskType : byte
 	{
-		None = 0,
 		OnlyDigits = 1,
 		Custom = 128
 	}
-	public sealed class InputMaskBehavior : Behavior<TextBox>
+	public sealed class InputHandleBehavior : Behavior<TextBox>
 	{
-
-
-
-
 		public MaskType MaskType
 		{
 			get { return (MaskType)GetValue(MaskTypeProperty); }
 			set { SetValue(MaskTypeProperty, value); }
 		}
-
-		// Using a DependencyProperty as the backing store for MaskType.  This enables animation, styling, binding, etc...
 		public static readonly DependencyProperty MaskTypeProperty =
-			DependencyProperty.Register("MaskType", typeof(MaskType), typeof(InputMaskBehavior), new PropertyMetadata(0));
-
-
-
-
+			DependencyProperty.Register("MaskType", typeof(MaskType), typeof(InputHandleBehavior), new PropertyMetadata(MaskType.Custom));
 
 		public string? CustomRegex
 		{
 			get { return (string?)GetValue(CustomRegexProperty); }
 			set { SetValue(CustomRegexProperty, value); }
 		}
-
-		// Using a DependencyProperty as the backing store for CustomRegex.  This enables animation, styling, binding, etc...
 		public static readonly DependencyProperty CustomRegexProperty =
-			DependencyProperty.Register("CustomRegex", typeof(string), typeof(InputMaskBehavior), new PropertyMetadata(null));
+			DependencyProperty.Register("CustomRegex", typeof(string), typeof(InputHandleBehavior), new PropertyMetadata(null));
+
+		#region lazy static regex
+		private static readonly Lazy<Regex> _numRegex = new(() => new Regex(@"^[0-9]+$", RegexOptions.Compiled), System.Threading.LazyThreadSafetyMode.ExecutionAndPublication);
+		#endregion //lazy static regex
 
 
-
-
-
-		private static readonly Lazy<Regex> _numRegex = new(() =>
-		new Regex("ss"),
-			System.Threading.LazyThreadSafetyMode.ExecutionAndPublication);
-
-		private Regex _regex;
+		private Regex _regex = null!;
 		protected override void OnAttached()
 		{
 			base.OnAttached();
-			AssociatedObject.PreviewTextInput += Filter_Input;
+
 			_regex = MaskType switch
 			{
 				MaskType.OnlyDigits => _numRegex.Value,
+				MaskType.Custom => new(CustomRegex ?? throw new ArgumentNullException(nameof(CustomRegex), "No custom regex string.")),
 				_ => throw new ArgumentException("No mask type", nameof(MaskType)),
 			};
+			AssociatedObject.PreviewTextInput += Filter_Input;
+			DataObject.AddPastingHandler(AssociatedObject, Filter_paste);
+		}
+
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+
+			AssociatedObject.PreviewTextInput -= Filter_Input;
+			DataObject.RemovePastingHandler(AssociatedObject, Filter_paste);
+		}
+		private void Filter_paste(object sender, DataObjectPastingEventArgs e)
+		{
+			if (e.SourceDataObject.GetData(DataFormats.UnicodeText, false) is string paste_string)
+			{
+				if (!_regex.IsMatch(paste_string))
+				{
+					e.CancelCommand();
+				}
+			}
 		}
 
 		private void Filter_Input(object sender, System.Windows.Input.TextCompositionEventArgs e)
 		{
-			e.Handled = _regex.IsMatch(e.Text);
+			if (e.Handled) return;
+			else if (!_regex.IsMatch(e.Text))
+			{
+				e.Handled = true;
+			}
 		}
 	}
 }
