4db66e7c63762cd0bfa05e7e048892fe14f80a65
qtoyam <qtoyam@gmail.com>
2022-05-13 14:12:48 +0300

    Add LoadingControlEx

---
+++ WPFCoreEx/Controls/LoadingControlEx.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFCoreEx.Controls
+{
+	public class LoadingControlEx : Control
+	{
+		static LoadingControlEx() =>
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(LoadingControlEx),
+				new FrameworkPropertyMetadata(typeof(LoadingControlEx)));
+	}
+}
